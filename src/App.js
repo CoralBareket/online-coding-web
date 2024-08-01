74ec19b4b8c06d62a3a355f20b9e816dc3be7b41
CoralBareket <coral.bareket@gmail.com>
2024-08-01 14:49:28 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState,useEffect } from 'react';
+import Lobby from './components/Lobby';
+import CodeBlock from './components/CodeBlock';
+
+const App = () => {
+  const [selectedCodeBlock, setSelectedCodeBlock] = useState(null);
+
+
+   useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.clear();
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  return (
+    <div>
+      {!selectedCodeBlock ? (
+         <Lobby setSelectedCodeBlock={setSelectedCodeBlock} />
+      ) : (
+        <CodeBlock title={selectedCodeBlock} />
+      )}
+    </div>
+  );
+};
+
+export default App;
